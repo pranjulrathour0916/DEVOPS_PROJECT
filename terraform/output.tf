33003b21aaae987bcabcaa3bb657f8a51b5703ba
pranjulrathour0916 <pranjalrathore9838@gmail.com>
2026-07-12 13:32:20 +0530

    changing image

--- terraform/output.tf
+++ terraform/output.tf
@@ -18,14 +18,14 @@ output "backend_ecr_repo" {
  value = aws_ecr_repository.backend.repository_url
 }
 
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
+# output "cluster_name" {
+#   value = module.eks.cluster_name
+# }
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
+# output "cluster_endpoint" {
+#   value = module.eks.cluster_endpoint
+# }
 
-output "cluster_ca_certificate" {
-  value = module.eks.cluster_certificate_authority_data
-}
+# output "cluster_ca_certificate" {
+#   value = module.eks.cluster_certificate_authority_data
+# }
